9ac6ff72767121351682e7a68552148e9ebc22c8
Park-Young-Hun <55652627+Park-Young-Hun@users.noreply.github.com>
2020-11-22 13:51:10 +0900

    Framework
    framework(2020.11.21)

---
+++ master/website/config/views.py
@@ -0,0 +1,21 @@
+from django.views.generic import TemplateView
+
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+
+# Main View
+class MainView(TemplateView):
+    template_name = 'login.html'
+
+
+# User Creation
+class UserCreateView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registration/register_done.html'
