816b5979a02d6fcff1a3f31e94187eb4c1764876
H_JJUNL <50603209+hjjunl@users.noreply.github.com>
2021-01-06 16:52:39 +0900

    ㄴ

--- master/website/board/admin.py
+++ master/website/board/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-=======
-# from .models import Corporation, Establishment, Department, Factory, Workshop, Item, CostCenter, CostElementAccount, \
-#     Client, BOM
->>>>>>> Stashed changes
 
 from .models import *
 
-<<<<<<< Updated upstream
-# from .models import Corporation, Establishment, Department, Factory, Workshop, Item, CostCenter, CostElementAccount, \
-#     Client, BOM
 
 
 
@@ -123,75 +115,3 @@ class CbCostCenterAdmin(admin.ModelAdmin):
     #list_filter = ('updt_dt',)
     # search_fields = ('name', 'content')
 
-=======
-# Register your models here.
-
->>>>>>> Stashed changes
-# @admin.register(Corporation)
-# @admin.register(Establishment)
-# @admin.register(Department)
-# @admin.register(Factory)
-# @admin.register(Workshop)
-# @admin.register(Item)
-# @admin.register(CostCenter)
-# @admin.register(CostElementAccount)
-# @admin.register(Client)
-# @admin.register(BOM)
-# class CorporationAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class EstablishmentAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class DepartmentAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class FactoryAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class WorkshopAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class ItemAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class CostCenterAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class CostElementAccountAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class ClientAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
-#
-#
-# class BOMAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name', 'modify_dt')
-#     list_filter = ('modify_dt',)
-#     search_fields = ('name', 'content')
