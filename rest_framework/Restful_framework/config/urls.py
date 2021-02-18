99e2713ef84bded1214698999d75acf3099d2325
Park-Young-Hun <55652627+Park-Young-Hun@users.noreply.github.com>
2021-02-18 22:40:36 +0900

    Add Bco table CRUD function
    Add Bco table CRUD function

--- rest_framework/Restful_framework/config/urls.py
+++ rest_framework/Restful_framework/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('rest_api', include('rest_api.urls')),
+    path('rest_api/', include('rest_api.urls')),
 ]
