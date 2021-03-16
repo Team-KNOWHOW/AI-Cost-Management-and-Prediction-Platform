6275df0640ebdf9fd532e97ea233c4c5d716b082
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-03-16 23:44:01 +0900

    Add Swagger
    Add Swagger by drf_yasg lib.

--- rest_framework/Restful_framework/config/urls.py
+++ rest_framework/Restful_framework/config/urls.py
@@ -15,8 +15,41 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_url_patterns = [
+    path('rest_api/', include('rest_api.urls')),
+]
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="KNOWHOW Swagger",
+        default_version="v1",
+        description="KNOWHOW Rest api 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="pyhpyh0670@gmail.com"),
+        license=openapi.License(name="GNU General Public License v2.0"),
+    ),
+    validators=['flex'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    patterns=schema_url_patterns,
+)
+
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('rest_api/', include('rest_api.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = [
+        path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
