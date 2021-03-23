f5aa7b358e3f32d14bdde699667a0b7f438c1de9
Young Hun Park <55652627+Park-Young-Hun@users.noreply.github.com>
2021-03-23 21:17:20 +0900

    Update urls.py
    Update urls.py.

--- rest_framework/Restful_framework/config/urls.py
+++ rest_framework/Restful_framework/config/urls.py
@@ -52,4 +52,5 @@ if settings.DEBUG:
         path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name="schema-json"),
         path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        path('rest_api/', include('rest_api.urls')),
     ]
